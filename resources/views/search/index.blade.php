ecf969a6272bd81c70d422badd8d46056d11026f
tashi1118 <tech.tashi1118.1991@gmail.com>
2020-03-23 19:55:58 +0900

    検索結果表示用viewの作成

---
+++ resources/views/search/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.common')
+
+@section('pageCss')
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+@endsection
+
+@section('content')
+
+    <div class="container">
+        @if(session('msg_success'))
+            <div class="alert-success">
+                {{ session('msg_success') }}
+            </div>
+        @endif
+
+        <div class="contents"> <!-- float:left; -->
+            <div class="issue-list">
+                <h2 class="issue-list__title l-size">{{ '「'.$word.'」' }}の検索結果：{{ $count }}件</h2>
+                <div class="cf">
+                    <article>
+                        <ul>
+                            @if($posts->isNotEmpty())
+                                @foreach ($posts as $post)
+                                    <li class="issue-item">
+                                        <dl>
+                                            <dt class="iconBox">
+                                                @if ($post->user->photo_id)
+                                                    <img class="issue-item__icon" src="{{ Storage::disk('s3')->url('profile/'. $post->user->photo->filename) }}" alt="ユーザー画像">
+                                                @else
+                                                    <img class="issue-item__icon" src="{{ asset('img/no_image.png') }}" alt="ユーザー画像">
+                                                @endif
+                                            </dt>
+                                            <dd class="infoBox">
+                                                <h2 class="underline issue-item__title m-size">
+                                                    <a href="{{ url('/post/'.$post->id ) }}">{{ str_limit($post->title, 70) }}</a>
+                                                </h2>
+                                                <span class="issue-item__subdescription">by {{ $post->user->name }}</span>
+                                                <span class="issue-item__subdescription">{{ $post->user->division }}</span>
+                                                <span class="issue-item__subdescription">{{ $post->created_at->format('Y.m.d(D) H:i') }}</span>
+                                                <span class="issue-item__subdescription">{{ $post->category->name }}</span>
+                                                <p class="issue-item__description s-size">{{ str_limit($post->description, 110) }}</p>
+                                                <a class="s-size" href="{{ url('/post/'.$post->id ) }}">...続きを読む</a>
+                                            </dd>
+                                        </dl>
+                                    </li>
+                                @endforeach
+                                {{ $posts->appends(request()->input())->links() }}
+                            @else
+                                <p class="m-size">検索結果はありません</p>
+                            @endif
+                        </ul>
+                    </article>
+                </div>
+            </div>
+        </div>
+
+        @component('components.sidebar')
+        @endcomponent
+
+    </div>
+@endsection
