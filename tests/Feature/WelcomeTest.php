6e8d90b4e149de63c525af76b2e88d794bf5ca59
tashi1118 <tech.tashi1118.1991@gmail.com>
2020-05-08 16:53:00 +0900

    テストコード追加、メソッド分け

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -2,24 +2,56 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class WelcomeTest extends TestCase
 {
+
+     // テスト実行後にDBロールバック
+     use RefreshDatabase;
+
     /**
-     * A basic feature test example.
+     * welcomeページ　表示テスト
      *
      * @return void
      */
-    public function testExample()
+    public function testWelcome()
     {
+        $this->withoutExceptionHandling();
+
         // welcomeページ正常表示チェック
         $response = $this->get('/');
 
         $response->assertStatus(200)
             ->assertViewIs('welcome')
-            ->assertSee('BizIT');
+            ->assertSee('「ITやデジタルは難しい。誰に聞けば…。」');
+    }
+
+
+    /**
+     * ログイン後welcomeページ　ヘッダー表示テスト
+     *
+     * @return void
+     */
+    public function testWelcomeHeaderAuth()
+    {
+
+        $this->withoutExceptionHandling();
+
+        // テストDBにダミーユーザー作成、Seederと同様
+        $user = factory(User::class)->create();
+
+        // ログイン後welcomeページヘッダーチェック
+        $response = $this
+            ->actingAs($user)
+            ->get(route('welcome'));
+        // チェック
+        $response->assertStatus(200)
+            ->assertViewIs('welcome')
+            ->assertSee('ログアウト');
+
     }
 }
